4f41efd9c61af9e9ec0a4dbd923d7169ae6f4adc
Maximus-98 <61884248+Maximus-98@users.noreply.github.com>
2020-03-06 15:50:25 -0500

    Add files via upload

---
+++ RandomMooValue.java
@@ -0,0 +1,116 @@
+package sample;
+
+public class RandomMooValue {
+
+    public RandomMooValue(){// Creates a new RandomMooValue object containing a secret value to be guessed
+
+    }
+
+    // Variables
+
+    public static String secretvalue;
+
+// String.valueOf(temp.charAt(i)).equals(String.valueOf(secretvalue.charAt(j)))
+    // Methods
+
+    /**getBigMooCount
+     *The number of digits in the user's guess that exactly (i.e., same position) matches digits in the secret value.
+     * @param guess - The number that the user guessed.
+     * @return a number 0-4 representing how many digits the user guessed correctly by position.
+     */
+    public int getBigMooCount(int guess) {
+        int bigMoo = 0;
+        String temp = String.valueOf(guess);
+        for (int i = 0; i < temp.length(); i++) {
+            for (int j = 0; j < secretvalue.length(); j++) {
+                if (String.valueOf(temp.charAt(i)).equals(String.valueOf(secretvalue.charAt(j)))) {  // if its the same value for one of the values...
+                    if (i == j) bigMoo++;     // ...and if its in the same place as that value... increment bigMoo.
+                }
+            }
+        }
+        return bigMoo;
+    }
+
+    /**getLittleMooCount
+     *The number of digits in the user's guess that exactly (i.e., same position) matches digits in the secret value.
+     * @param guess - The number that the user guessed.
+     * @return a number 0-4 representing how many of the guessed digits match, but are in different
+     * positions. Note that a guessed number cannot have any one digit generate both a "MOO!"
+     * and a "moo." as a result.
+     */
+    public int getLittleMooCount(int guess){
+        int littleMoo = 0;
+        String temp = String.valueOf(guess);
+        for (int i = 0; i < temp.length(); i++) {
+            for (int j = 0; j < secretvalue.length(); j++) {
+                if (String.valueOf(temp.charAt(i)).equals(String.valueOf(secretvalue.charAt(j)))) {   // if its the same value for one of the values...
+                    if (i != j) littleMoo++;    // ...and if its NOT in the same place as that value... increment littleMoo.
+                }
+            }
+        }
+        int littleTruMoo = littleMoo - getBigMooCount(guess);
+
+        return littleTruMoo;
+    }
+
+    /**getSecretValue
+     *Access the secret value that the user is trying to guess, primarily to show the user after running out of guesses.
+     * @return the secret value that the user is/was attempting to guess.
+     */
+    public static int getSecretValue(){
+        return Integer.parseInt(secretvalue);
+    }
+
+    /**isCorrectGuess
+     *Determines if the user correctly guessed the secret value.
+     * @param guess - The number that the user guessed.
+     * @return true if the guess is correct, false otherwise.
+     */
+    public boolean isCorrectGuess(int guess){
+        String temp = String.valueOf(guess);
+        for (int i = 0; i < 4; i++) {
+            if (!(String.valueOf(temp.charAt(i)).equals(String.valueOf(secretvalue.charAt(i))))){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**setSecretValue
+     * Generates a new secret value to be guessed in a game of LaurieMOO
+     * @return true
+     */
+    public boolean setSecretValue(){
+        int digit1, digit2, digit3, digit4;
+        digit1 = (int) (Math.random() * 10);
+        digit2 = (int) (Math.random() * 10);
+        digit3 = (int) (Math.random() * 10);
+        digit4 = (int) (Math.random() * 10);
+        secretvalue = String.valueOf(digit1) + digit2 + digit3 + digit4;
+        return true;
+    }
+
+    /**setSecretValue
+     *Generates a new secret value to be guessed in a game of LaurieMOO.
+     * @param n - The number that will be set as the secret value, if it is within the inclusive
+     * range of 0000-9999.
+     * @return true if the secret value was reset; false if the passed value was outside of the
+     * allowed range of values.
+     */
+    public boolean setSecretValue(int n){
+        String temp = String.valueOf(n);
+        if (temp.length() == 4) {
+            secretvalue = temp;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isValid(int n){
+        String temp = String.valueOf(n);
+        return temp.length() == 4;
+    }
+
+}
+
+
